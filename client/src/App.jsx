77a3ed30cce281da84ff4beca1085b6933c7fdca
Nikolay Nikolaev <nikolaev.a.nikolay@gmail.com>
2025-12-02 21:03:47 +0200

    chore: add zustand; add auth-store; add repice-store

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,28 @@
 import { Center, HStack, Button } from "@chakra-ui/react";
-import React from 'react';
+import React, { useCallback } from 'react';
 import { ColorModeButton } from "./components/ui/color-mode";
+import { useAuthStore } from "./store/auth-store";
 
 const App = () => {
+  const { user, isAuthenticated, login, logout } = useAuthStore();
+  const fakeLogin = useCallback(() => {
+    login({ id: 1, name: "John Doe", email: "john.doe@gmail.com" }, "fake-jwt-token");
+  }, [login]);
+
   return (
-   <>
-    <ColorModeButton value="Change theme" data-testid="change-theme" />
-   </>
+    <>
+      <Center>
+        {isAuthenticated
+          ? (
+            <>
+              <HStack>{JSON.stringify(user, null, 2)}</HStack>
+              <HStack><Button onClick={logout}>Logout</Button></HStack>
+            </>
+          )
+          : (<Button onClick={fakeLogin}>Fake Login</Button>)}
+      </Center>
+      <ColorModeButton value="Change theme" data-testid="change-theme" />
+    </>
   )
 }
 
---
+++ client/src/store/recipe-store.js
@@ -0,0 +1,26 @@
+import { create } from "zustand";
+
+export const useRecipeStore = create((set) => ({
+    recipes: [],
+    loading: false,
+    error: null,
+
+    setRecipes: (recipes) => set({ recipes }),
+
+    addRecipe: (recipe) =>
+        set((state) => ({
+            recipes: [...state.recipes, recipe],
+        })),
+
+    updateRecipe: (updatedRecipe) =>
+        set((state) => ({
+            recipes: state.recipes.map((r) =>
+                r._id === updatedRecipe._id ? updatedRecipe : r
+            ),
+        })),
+
+    deleteRecipe: (id) =>
+        set((state) => ({
+            recipes: state.recipes.filter((r) => r._id !== id),
+        })),
+}));
